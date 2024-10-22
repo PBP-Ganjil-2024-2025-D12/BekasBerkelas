5a61edbbd3b3c258785cdd99fb389083653de428
StevenSetiawan <stevensetiawan1802@gmail.com>
2024-10-22 18:13:34 +0700

    navbar done for non-login user

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,20 @@
-const btn = document.querySelector(".mobile-menu-button");
-const menu = document.querySelector(".mobile-menu");
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector(".mobile-menu-button");
+    const menu = document.querySelector(".mobile-menu");
+    const hamburgerIcon = document.querySelector(".hamburger-icon");
+    const xIcon = document.querySelector(".x-icon");
 
-btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
+    if (btn) {
+        btn.addEventListener("click", () => {
+            menu.classList.toggle("hidden");
+            
+            if (menu.classList.contains("hidden")) {
+                hamburgerIcon.classList.remove("hidden");
+                xIcon.classList.add("hidden");
+            } else {
+                hamburgerIcon.classList.add("hidden");
+                xIcon.classList.remove("hidden");
+            }
+        });
+    }
 });
