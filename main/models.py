323d8a9fae8f8973c3e05253c52f5024626b8d33
StevenSetiawan <stevensetiawan1802@gmail.com>
2024-10-22 09:55:25 +0700

    feat: add user models and form

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserRole(models.TextChoices) :
+    ADMIN = 'ADM', 'Admin',
+    BUYER = 'BUY', 'Buyer',
+    SELLER = 'SEL', 'Seller'
+    
+class UserProfile(models.Model) :
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    email = models.EmailField()
+    role = models.CharField(default=UserRole.BUYER, choices=UserRole.choices, max_length=3)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
