23a58b0167bd5ef0f3fc0cfbf40785159fff5be7
StevenSetiawan <stevensetiawan1802@gmail.com>
2024-10-23 11:33:08 +0700

    remove JWT

--- authentication/views.py
+++ authentication/views.py
@@ -1,16 +1,14 @@
 import datetime
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-from jwt_auth.utils import set_jwt_cookies, clear_jwt_cookies
 
 # Create your views here.
 def register_user(request) :
     form = RegisterForm()
-    access_token = request.COOKIES.get('access_token')
     
-    if access_token :
+    if request.user.is_authenticated :
         return redirect('main:main')
     
     if request.method == "POST" :
@@ -23,8 +21,7 @@ def register_user(request) :
     return render(request, 'register.html', context)
 
 def login_user(request) :
-    access_token = request.COOKIES.get('access_token')
-    if access_token :
+    if request.user.is_authenticated :
         return redirect('main:main')
     
     if request.method == 'POST' :
@@ -33,12 +30,14 @@ def login_user(request) :
         user = authenticate(request, username = username, password = password)
         
         if user is not None :
+            login(request, user)
             next = request.GET.get('next')
             if next is not None :
                 response = redirect(next)
             else :
                 response = redirect('main:main')
-            return set_jwt_cookies(response, user)
+            response.set_cookie('user_login', user)
+            return response
         else :
             messages.error(request, 'Invalid username or password')
             return redirect('authentication:login')
@@ -46,5 +45,7 @@ def login_user(request) :
     return render(request, 'login.html')
 
 def logout_user(request) :
+    logout(request)
     response = redirect('authentication:login')
-    return clear_jwt_cookies(response)
+    response.delete_cookie('user_login')
+    return response
