bcd1b5d6f6d002534ae25b5e8537671b3e649aff
Ramy Ardya <161923530+UltraRamy@users.noreply.github.com>
2024-10-23 15:34:18 +0700

    Update models.py

--- product_catalog/models.py
+++ product_catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    CAR_TRANSMISSION_CHOICES = [
+        ('Manual', 'Manual'),
+        ('Automatic', 'Automatic'),
+    ]
+    
+    PLATE_TYPE_CHOICES = [
+        ('Even', 'Even'),
+        ('Odd', 'Odd'),
+    ]
+    
+    car_name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    mileage = models.PositiveIntegerField()
+    location = models.CharField(max_length=100)
+    transmission = models.CharField(max_length=10, choices=CAR_TRANSMISSION_CHOICES)
+    plate_type = models.CharField(max_length=10, choices=PLATE_TYPE_CHOICES)
+    rear_camera = models.BooleanField(default=False)
+    sun_roof = models.BooleanField(default=False)
+    auto_retract_mirror = models.BooleanField(default=False)
+
