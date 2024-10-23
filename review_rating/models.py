b9ccbc86f9939963e5f351f46536adc0f3d55526
widaputri <widapk2709@gmail.com>
2024-10-24 03:06:40 +0700

    feat: review card (with dummy data)

--- review_rating/models.py
+++ review_rating/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 class ReviewRating(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     review = models.TextField()
     rating = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
--- review_rating/tests.py
+++ review_rating/tests.py
@@ -5,7 +5,6 @@ from .models import ReviewRating
 class ReviewRatingTest(TestCase):
 
     def setUp(self):
-        # Set up a user and a review for testing
         self.user1 = User.objects.create_user(username='user1', password='pass1234')
         self.user2 = User.objects.create_user(username='user2', password='pass1234')
 
@@ -25,10 +24,8 @@ class ReviewRatingTest(TestCase):
         self.assertEqual(review.reviewee, self.user2)
 
     def test_review_rating_range(self):
-        # Test if rating is within the correct range
         self.assertGreaterEqual(self.review.rating, 1)
         self.assertLessEqual(self.review.rating, 5)
 
     def test_string_representation(self):
-        # Optionally, you might want to test the string representation of the model
         self.assertEqual(str(self.review), "Great car!")
