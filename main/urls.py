685b3ed3c3f2e02cb421ea7bc755b37cf3696dfb
deanitasekar <deanitasekar@gmail.com>
2024-10-23 20:08:15 +0700

    wishlist

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from main.views import main
+from wishlist.views import add_to_wishlist, show_wishlist, remove_from_wishlist
 
 app_name = 'main'
 
 urlpatterns = [
     path('', main, name='main'),
+    path('', show_wishlist, name='wishlist_page'),
+    path('add/<int:car_id>/', add_to_wishlist, name='add_to_wishlist'),
+    path('remove/<int:car_id>/', remove_from_wishlist, name='remove_from_wishlist'),
 ]
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from product_catalog.models import Car
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return f"{self.car.car_name}"
