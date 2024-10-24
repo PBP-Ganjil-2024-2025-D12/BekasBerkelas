11b682fbf1e32353b3e9a1317d558963bd05539a
deanitasekar <deanitasekar@gmail.com>
2024-10-24 09:16:38 +0700

    updating wishlist

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,4 @@ app_name = 'main'
 
 urlpatterns = [
     path('', main, name='main'),
-    path('', show_wishlist, name='wishlist_page'),
-    path('add/<int:car_id>/', add_to_wishlist, name='add_to_wishlist'),
-    path('remove/<int:car_id>/', remove_from_wishlist, name='remove_from_wishlist'),
 ]
