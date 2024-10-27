39c1338192704f16d66af48b763013eb952fe909
Steven Setiawan <135805488+setiawans@users.noreply.github.com>
2024-10-27 23:54:19 +0700

    Update populate.py

--- populate.py
+++ populate.py
@@ -1,12 +1,11 @@
 import os
 import django
 
-# Set the settings module (replace 'yourproject.settings' with your actual settings module)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bekas_berkelas.settings')
 django.setup()
 
 from django.contrib.auth.models import User
-from authentication.models import UserProfile, UserRole  # Replace 'yourapp' with your app's name
+from authentication.models import UserProfile, UserRole 
 
 names = [
     "Ramy", "Ultramy", "UltraRamy", "Naufal", "Muhammad",
@@ -17,22 +16,20 @@ names = [
 password = 'BekasBerkelas'
 
 for name in names:
-    username = name.lower()  # Convert to lowercase for username
+    username = name.lower()  
     email = f"{username}@gmail.com"
 
-    # Create User
     user = User.objects.create_user(username=username, email=email, password=password)
 
-    # Create UserProfile with is_verified=True and role=SELLER
     UserProfile.objects.create(
         user=user,
         name=name,
         email=email,
         no_telp='1234567890',
-        role=UserRole.SELLER,  # Set role as SELLER
+        role=UserRole.SELLER,  
         profile_picture=None,
         profile_picture_id=None,
-        is_verified=True  # Set verified to True
+        is_verified=True 
     )
 
 print("Created user profiles with all verified and role as Seller.")
