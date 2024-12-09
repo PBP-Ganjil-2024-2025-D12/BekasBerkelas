80666439abf68a51feb5419a0608fe4e6b6ea1c4
StevenSetiawan <stevensetiawan1802@gmail.com>
2024-12-09 13:54:33 +0700

    update abit to integrate with dart

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
-import datetime
+import json
+from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.conf import settings
@@ -6,58 +7,75 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
 @csrf_exempt
-def register_user(request) :
-    form = RegisterForm()
-    
-    if request.user.is_authenticated :
-        return redirect('main:main')
-    
-    if request.method == "POST" :
-        form = RegisterForm(request.POST)
-        if form.is_valid() :
+def register_user(request):
+    if request.method == "POST":
+        data = request.POST
+        print(data)
+        
+        form = RegisterForm(data)
+        if form.is_valid():
             if form.cleaned_data['role'] == 'ADM' and form.cleaned_data['password1'] != settings.ADMIN_ACCOUNT_SECRET_TOKEN:
-                messages.error(request, 'Invalid Admin Credentials')
-                return redirect('authentication:register')
+                return JsonResponse({
+                    'status': 'failed',
+                    'message': 'Invalid Admin Credentials'
+                }, status=400)
             
             form.save()
-            messages.success(request, 'Your account has been successfully created!')
-            return redirect('authentication:login')
-        else :
-            messages.error(request, 'Failed to create account')
-            return redirect('authentication:register')
-        
-    context = {'form':form}
+            return JsonResponse({
+                'status': 'success',
+                'message': 'Your account has been successfully created!'
+            }, status=200)
+        else:
+            return JsonResponse({
+                'status': 'failed',
+                'message': 'Failed to create account',
+                'errors': form.errors
+            }, status=400)
+            
+    form = RegisterForm()
+    context = {'form': form}
     return render(request, 'register.html', context)
 
 @csrf_exempt
-def login_user(request) :
-    if request.user.is_authenticated :
-        return redirect('main:main')
-    
-    if request.method == 'POST' :
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username = username, password = password)
-        
-        if user is not None :
-            login(request, user)
-            next = request.GET.get('next')
-            if next is not None :
-                response = redirect(next)
-            else :
-                response = redirect('main:main')
-            response.set_cookie('user_login', user)
-            return response
-        else :
-            messages.error(request, 'Invalid username or password')
-            return redirect('authentication:login')
+def login_user(request):
+    if request.method == 'POST':
+        try:
+            if request.content_type == 'application/json':
+                data = json.loads(request.body)
+                username = data.get('username')
+                password = data.get('password')
+            else:
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return JsonResponse({
+                    "status": "success",
+                    "message": "Successfully logged in!",
+                    "username": user.username,
+                }, status=200)
+            else:
+                return JsonResponse({
+                    "status": "failed",
+                    "message": "Invalid username or password"
+                }, status=401)
+                
+        except json.JSONDecodeError:
+            return JsonResponse({
+                'status': 'failed',
+                'message': 'Invalid JSON data'
+            }, status=400)
 
     return render(request, 'login.html')
 
-def logout_user(request) :
+@csrf_exempt
+def logout_user(request):
     logout(request)
-    response = redirect('main:main')
-    response.delete_cookie('user_login')
-    return response
+    return JsonResponse({
+        "status": "success",
+        "message": "Successfully logged out!"
+    }, status=200)
