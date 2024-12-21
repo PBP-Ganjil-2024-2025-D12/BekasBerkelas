ba03654967afe365ac21f1b4c9d37a8245e7c765
Ramy Ardya <161923530+UltraRamy@users.noreply.github.com>
2024-12-21 18:02:59 +0700

    Kelupaan Delete

--- product_catalog/urls.py
+++ product_catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from product_catalog.views import car_list, create_car, delete_car, mobil_saya, view_details_json, contact_seller, view_details, show_all_cars, create_car_flutter, show_user_profile_json, filter_cars
+from product_catalog.views import car_list, create_car, delete_car, mobil_saya, contact_seller, view_details, show_all_cars, create_car_flutter, show_user_profile_json, filter_cars, search_filter_cars,delete_flutter
 
 app_name = 'product_catalog'
 
@@ -11,8 +11,9 @@ urlpatterns = [
     path('delete_car/<uuid:car_id>/', delete_car, name='delete_car'),
     path('car/<uuid:car_id>/contact/', contact_seller, name='contact_seller'),
     path('detail/<uuid:car_id>/', view_details, name='view_details'),
-    path('detail/json/<uuid:car_id>/', view_details_json, name='view_details_json'),
     path('create-flutter/', create_car_flutter, name='create_flutter'),
     path('api/user-profile/', show_user_profile_json, name='show_user_profile_json'),
     path('api/mobilsaya/', filter_cars, name='filter_cars'),
+    path('api/cars/filter/', search_filter_cars, name='filter_cars_json'),
+    path('api/mobilsaya/delete/', delete_flutter, name='delete_flutter'),
 ]
